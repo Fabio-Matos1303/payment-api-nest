7b9cdf8f073abd8859ab96bfbea43212ad146870
root <fabiomatos@infojr.com.br>
2024-04-08 22:10:21 -0300

    feat: create docker-compose and sets database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   // Importar informações doSevice de módulo
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${(process.env.NODE_ENV, 'development')}`],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345678',
+      database: 'postgres',
+    }),
+    UserModule,
+  ],
   // Controllers: Verbos
   controllers: [],
   // Services: Lógica
